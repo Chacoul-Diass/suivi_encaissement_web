b63720d0aeffc0ab1d24ea91305dc4695b91bdfc
YoannWilson225 <yoannphilippewilson@gmail.com>
2025-04-23 09:13:58 +0000

    recup main

--- components/litige/litige.tsx
+++ components/litige/litige.tsx
@@ -209,7 +209,6 @@ export default function Litige() {
                     total={Totaldata}
                     paginate={paginate}
                     loading={loading}
-                    fetchData={fetchData}
                     habilitation={habilitation}
                     handlePageChange={handlePageChange}
                     handleSearchChange={handleSearchChange}
