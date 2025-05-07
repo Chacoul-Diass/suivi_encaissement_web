a32be4e6f91af0aedbf511ebef38834c09641612
Yacine8800 <71154342+Yacine8800@users.noreply.github.com>
2025-05-07 14:44:40 +0000

    Refactor session expiration handling in axios interceptor. Replaced modal implementation with an alert system to improve user experience. Updated state management for alert visibility and ensured proper logout process upon session expiration.

--- utils/axios.ts
+++ utils/axios.ts
@@ -15,57 +15,7 @@ const axiosInstance = axios.create({
   },
 });
 
-let isModalShown = false;
-
-// Fonction pour afficher une modal personnalisée au lieu d'une alerte
-const showSessionExpiredModal = () => {
-  // Créer un élément div pour la modal
-  const modalContainer = document.createElement("div");
-  modalContainer.className =
-    "fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50";
-
-  // Créer le contenu de la modal
-  const modalContent = document.createElement("div");
-  modalContent.className =
-    "w-[300px] rounded-lg bg-white p-6 text-center shadow-lg";
-
-  // Message
-  const messageElement = document.createElement("p");
-  messageElement.className = "mb-4 text-lg text-black";
-  messageElement.textContent = "Session expirée. Déconnexion en cours...";
-
-  // Bouton OK
-  const confirmButton = document.createElement("button");
-  confirmButton.className =
-    "hover:bg-primary-dark rounded-md bg-primary px-4 py-2 text-white";
-  confirmButton.textContent = "OK";
-
-  // Assembler la modal
-  modalContent.appendChild(messageElement);
-  modalContent.appendChild(confirmButton);
-  modalContainer.appendChild(modalContent);
-
-  // Ajouter la modal au document
-  document.body.appendChild(modalContainer);
-
-  // Gérer le clic sur le bouton
-  confirmButton.addEventListener("click", () => {
-    // Supprimer la modal
-    document.body.removeChild(modalContainer);
-
-    // Gérer la déconnexion
-    store.dispatch(logout());
-    localStorage.removeItem("persist:suivi-encaissement");
-    localStorage.removeItem("refresh_token");
-    deleteCookie("accessToken");
-    deleteCookie("refresh_token");
-    window.location.href = "/login";
-
-    setTimeout(() => {
-      isModalShown = false;
-    }, 5000);
-  });
-};
+let isAlertShown = false;
 
 axiosInstance.interceptors.request.use(
   (config) => {
@@ -112,9 +62,15 @@ const showSessionExpiredModal = () => {
 axiosInstance.interceptors.response.use(
   (response) => response.data,
   (error) => {
-    if (error.response?.status === 401 && !isModalShown) {
-      isModalShown = true;
+    if (error.response?.status === 401 && !isAlertShown) {
+      isAlertShown = true;
+
+      // Afficher un modal amélioré
       showSessionExpiredModal();
+
+      setTimeout(() => {
+        isAlertShown = false;
+      }, 5000);
     }
     return Promise.reject(error);
   }
